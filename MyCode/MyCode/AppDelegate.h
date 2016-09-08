40268beb11bdb04353e213142bfb250c0f1f8d01
horizon554 <jiangjiahao826@outlook.com>
2016-09-09 00:45:56 +0800

    myCode

---
+++ MyCode/MyCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCode
+//
+//  Created by JiangJiahao on 16/9/9.
+//  Copyright © 2016年 JiangJiahao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyCode/MyCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCode
+//
+//  Created by JiangJiahao on 16/9/9.
+//  Copyright © 2016年 JiangJiahao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
